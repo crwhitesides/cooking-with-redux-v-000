361bc7eb473f9798e546d62b7deeeeb4263e8c4b
Christian Whitesides <whitesidescr@gmail.com>
2017-06-09 18:20:00 -0600

    Done.

--- src/store.js
+++ src/store.js
@@ -2,7 +2,7 @@ import { createStore } from 'redux'
 import { rootReducer } from './reducers/index'
 
 export function configureStore(){
-  return createStore(rootReducer)
+  return createStore(rootReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()) // Second argument causes `learn` command to throw a ReferenceError
 }
 
 export const store = configureStore()
